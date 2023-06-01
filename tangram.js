5761a909393d11bce6d08f150cfee5b6e9c0e36f
Archimonde32 <dkgaming718@gmail.com>
2023-06-01 16:15:35 +0200

    miseaj3

--- tangram.js
+++ tangram.js
@@ -1,8 +1,16 @@
-const duck = new Audio('duck.mp3');
-var animal = this.id;
+// let tab =['desorder','square','cat','duck','rabbit','lion','kangaroo','horse','snake','turtle'];
+// let sons = new Audio();
+
 function swapStyleSheet(sheet) {
-    document.getElementById("stylepage").setAttribute("href",sheet);
-    duck.play();
+document.getElementById("stylepage").setAttribute("href",sheet);
+    // for (var i = 0; i < tab.length; i++) {
+    // tab[i].onclick = sons.src= "./sons/"+tab[i]+".mp3";
+
+    // }
+   
+    // sons.src= "./sons/"+tab[3]+".mp3";
+    // sons.play();
+
 }
 
 
