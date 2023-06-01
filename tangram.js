173961ef48aad8a7c037bc9cd792dbe692dbbb7d
Archimonde32 <dkgaming718@gmail.com>
2023-06-01 11:55:48 +0200

    sons

--- tangram.js
+++ tangram.js
@@ -1,3 +1,10 @@
+const duck = new Audio('duck.mp3');
+var animal = this.id;
 function swapStyleSheet(sheet) {
-    document.getElementById("stylepage").setAttribute("href",sheet);  
+    document.getElementById("stylepage").setAttribute("href",sheet);
+    duck.play();
 }
+
+
+
+
